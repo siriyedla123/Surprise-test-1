863a808ef4392e70edbe6aa6419fd222ed9451d8
siriyedla123 <90643444+siriyedla123@users.noreply.github.com>
2021-09-16 16:31:16 +0530

    quiz-1

---
+++ quiz1 2nd program.py
@@ -0,0 +1,5 @@
+a=input("Enter a line:")
+word= str(input())
+for word in a:
+    if word.count(5):
+        print(word) 
---
+++ quiz1 problem.py
@@ -0,0 +1,11 @@
+first_word=input("Enter the first_word:")
+second_word=input("Enter the second_word:")
+
+a=first_word[0]
+b=second_word[0]
+c=first_word[3]
+d=second_word[2]
+e=first_word[-1]
+f=second_word[-1]
+
+third_word= print(a+b+c+d+e+f)
