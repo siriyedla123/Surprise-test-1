e3703ab90fb52030796a85104f04d5618e87586c
siriyedla123 <90643444+siriyedla123@users.noreply.github.com>
2021-09-17 19:45:52 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,8 @@
+seta=set(['vamsi','mohan'])
+setb=set(['vamsi','mohan','prasanna'])
+if seta<=setb:
+    print("True")
+    print("seta is a subset of setb")
+else:
+     print("False")
+     print("seta is not a subset of setb")
